953744ee55ad19d0f98d15ecb7e1778b14215652
Alfredo Revilla <alfredorevilla@gmail.com>
2023-06-27 17:23:11 -0500

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _2fa_through_azure_ad_with__entity_id_and_acs_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2fa_through_azure_ad_with__entity_id_and_acs_.MvcApplication" Language="C#" %>
